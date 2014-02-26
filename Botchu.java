1caaf4a342d753509e88b1198cfd86437e19deb9
Bedard <bedkev01@ccm-2610-a1.evergreen.edu>
2014-02-25 16:57:53 -0800

    stuff

--- Botchu.java
+++ Botchu.java
@@ -12,12 +12,27 @@ public class Botchu extends PircBot {
 		if (message.equalsIgnoreCase("time")) {
 			String time = new java.util.Date().toString();
 			sendMessage(channel, sender + ": The time is now " + time);
-		if (message.equalsIgnoreCase("go away paulbot")) {
-			sendMessage("Goodbye " + sender + ", it has been fun.");
+			}
+	   if (message.equalsIgnoreCase("go away paul_bot")) {
+			sendMessage(channel, "Goodbye " + sender + ", it has been fun.");
 			disconnect();
+			}
+		if (message.equalsIgnoreCase("test")) {
+			sendMessage(channel, sender + " your test worked. Good Jorb.");
+			}
+/*		if (message.equalsIgnoreCase("Ascend")) {
+			changeNick(String alpha_paul_bot);
+			}
+/*
 
-			
-		}
+/* 
+
+the above is my next trick which is practicing changing paulbots nickname. 
+Once I am able to get that figured out, I will have a command "Paulbot become "x" where x is what paulbot will change thier nickname too. They can then use a command to be implimented later, "wheres my bot at?". 
+
+There is a raw class below that will be used for a dice roll opperation. I am currently trying to make the program try and take input from irc and use it as variables / strings to be used later. Once I figure out how to do this, I will be able to start getting some serious work done on PaulBot. 
+
+/*
 
 /*	public void onMessage(String channel, String sender, String login, String hostname, String message) {
 		
@@ -51,8 +66,9 @@ public class RollDice {
 
 */     
 
-	}
+
 	}
 
 
 	}
+
