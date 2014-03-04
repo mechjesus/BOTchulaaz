1b1d226db2802e89cda6ce5fa990b5f94c919728
Kevin <djinnpenguin@gmail.com>
2014-03-04 15:16:45 -0800

    Update Botchu.java
    added private message command support

--- Botchu.java
+++ Botchu.java
@@ -33,16 +33,7 @@ public class Botchu extends PircBot {
 //    joinChannel(channel);
 //}
 
-//TODO: Why do those two not work?
 
-/* 
-
-the above is my next trick which is practicing changing paulbots nickname. 
-Once I am able to get that figured out, I will have a command "Paulbot become "x" where x is what paulbot will change thier nickname too. They can then use a command to be implimented later, "wheres my bot at?". 
-
-There is a raw class below that will be used for a dice roll opperation. I am currently trying to make the program try and take input from irc and use it as variables / strings to be used later. Once I figure out how to do this, I will be able to start getting some serious work done on PaulBot. 
-
-/*
 
 /*	public void onMessage(String channel, String sender, String login, String hostname, String message) {
 		
@@ -78,7 +69,29 @@ public class RollDice {
 
 
 	}
+	public void onPrivateMessage(String channel, String sender, String login, String hostname, String message) {
+
+		if (message.equalsIgnoreCase("@time")) {
+			String time = new java.util.Date().toString();
+			sendMessage(channel, sender + ": The time is now " + time);
+			}
+	   	if (message.equalsIgnoreCase("@go away")) {
+			sendMessage(channel, "Goodbye " + sender + ", it has been fun.");
+			disconnect();
+			}
+		if (message.equalsIgnoreCase("@test")) {
+			sendMessage(channel, sender + " your test worked. Good Jorb.");
+			}
+		if (message.equalsIgnoreCase("@help")) {
+			sendMessage(sender, "Commands: time, fight, test, go away");
+			}
+		if (message.equalsIgnoreCase("@fight")) {
+			sendAction(channel, "establishes dominance over all other bots.");
+			}
+
+
+
 
 
 	}
-
+}
